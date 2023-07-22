cc5b52b4388e604fa2f7ff0e36b11031d95f85ce
tyt <tyt233233@gmail.com>
2023-07-22 21:45:02 +0800

    1.3学习类，命名空间Using语句使用

---
+++ Assets/_Script/Entity/Student.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace _Script.Entity
+{
+    public class Student
+    {
+        private int id;
+        private  string name;
+        public int ID
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        
+
+        public void Print()
+        {
+            Debug.Log($"{this.Name}");
+        }
+    }
+}
--- Assets/_Script/PlayerController.cs
+++ Assets/_Script/PlayerController.cs
@@ -1,21 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  // 将速度暴露出来，使其可调
-  public float speed = 0.1f;
-  // 每帧调用一次 Update
-  void Update()
-  {
-      float horizontal = Input.GetAxis("Horizontal");
-      float vertical = Input.GetAxis("Vertical");
-      Vector2 position = transform.position;
-      position.x = position.x + speed * horizontal * Time.deltaTime;
-      position.y = position.y + speed * vertical * Time.deltaTime;
-      transform.position = position;
+    private Rigidbody2D rigidBody;
+
+    private float horizontal;
+
+    float vertical;
+
+    // 将速度暴露出来，使其可调
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // 每帧调用一次 Update
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidBody.position = position;
         //////////////////////////////////////
-  }
-    
+    }
 }
